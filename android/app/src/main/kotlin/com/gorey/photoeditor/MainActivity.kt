5e5c19da48ddd45bc64141a318c243e663dd3205
pinkth3floyd <pinkth3floyd@gmail.com>
2024-06-20 19:35:41 +0545

    release v1

---
+++ android/app/src/main/kotlin/com/gorey/photoeditor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gorey.photoeditor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/AdConfigurationConstants.dart
@@ -0,0 +1,15 @@
+const disableAdMob = false;
+
+const String isGoogleAds = "admob";
+const String isFacebookAds = "facebook";
+
+const isAds = isGoogleAds;
+
+const mAdMobAppId = 'ca-app-pub-6352870695429233~8986614324';
+const mAdMobBannerId = 'ca-app-pub-6352870695429233/7127077059';
+const mAdMobInterstitialId = 'ca-app-pub-6352870695429233/5769424048';
+const mAdMobNativeAdvancedId = 'ca-app-pub-6352870695429233/3742365818';
+const mAdMobRewardedId = 'ca-app-pub-6352870695429233/5055447485';
+
+String faceBookBannerPlacementId = "";
+String faceBookInterstitialPlacementId = "";
